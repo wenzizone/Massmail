c001933e448adddcbef85f98554bd900403a9dbc
wenzizone <wenzizone@126.com>
2018-05-24 22:32:42 +0800

    no message

---
+++ frontend/src/components/Common/topMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-menu :default-active="activeIndex" mode="horizontal">
+    <el-menu-item v-for="menu in menuData" :index='menu.url' @click="routerChange(menu)">{{menu.title}}</el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  props: ['menuData'],
+  data () {
+    return {
+      activeIndex: this.$route.path
+    }
+  },
+  methods: {
+    routerChange (item) {
+      if (item.url !== this.$route.path) {
+        router.push(item.url)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-container>
+    <el-header height="auto">
+      <el-row>
+        <el-col :span="24"><h1>自助邮件群发系统</h1></el-col>
+      </el-row>
+      <topMenu :menuData="menuData" ref="topMenu"></topMenu>
+    </el-header>
+
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import topMenu from './Common/topMenu.vue'
+
+export default {
+  data () {
+    return {
+      menuData: []
+    }
+  },
+  created () {
+    let menus = [
+      { title: '发送邮件', url: '/' },
+      { title: '查看日志', url: '/logs' }
+    ]
+    this.menuData = menus
+  },
+  components: {
+    topMenu
+  }
+}
+</script>
+
+<style>
+  .el-header {
+    text-align: center;
+  }
+</style>
